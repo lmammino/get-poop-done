a48bf9fd69c7cb4c3907669b53d8e8be9bef9dd5
Luciano Mammino <lucianomammino@gmail.com>
2018-03-19 20:44:30 +0000

    Added version 2 and 3 and global build system

---
+++ build-all.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+CURRENT_PATH=`dirname "$(realpath $0)"`
+
+if [ -d "build" ]; then
+  rm -rf build
+  echo "👍  Cleaned up old build dir"
+fi
+
+TO_BUILD=(
+  "01-external-dependencies"
+  "02-concatenated-vendors"
+  "03-advanced-concatenated-vendors"
+)
+
+mkdir -p build
+
+for i in "${TO_BUILD[@]}"
+do
+  cd "${CURRENT_PATH}/src/${i}"
+  npm install
+  npm run build
+  echo "👍  Built ${i}"
+
+	cd "${CURRENT_PATH}"
+done
+
+echo ""
+echo "👍  Completed!"
